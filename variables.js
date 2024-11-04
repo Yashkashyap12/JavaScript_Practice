b6bb74998fac1bf2c4b1dac0f2cf37307409f787
Yash Kashyap <gyashkashyap123456@gmail.com>
2024-11-04 20:28:07 +0530

    first

---
+++ variables.js
@@ -0,0 +1,17 @@
+const accountId = 6562542
+let accountEmail = "radha@gmail.com"
+var accountPass = "radha5752"
+accountCity = "Kanpur"
+let accountState;
+
+//accountID = 2 //not allowed here
+
+accountEmail = "yash@gmail.com"
+accountPass = "yash1727"
+accountCity = "Ghaziabad"
+
+//console.log(accountId);
+
+//Prefer not to say because of issue in block scope and functional scope
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
