a6d031fc0fbd838436928b85d0c949c8ce62eb1a
Yash Kashyap <gyashkashyap123456@gmail.com>
2024-11-06 19:37:25 +0530

    newfileadd

---
+++ nums_and_maths.js
@@ -0,0 +1,41 @@
+// *************** Numbers ************************
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNum = 26.66541
+// console.log(otherNum.toPrecision(3));
+
+// const hundred = 10000
+// console.log(hundred.toLocaleString('en-IN'));
+
+// ********************** Maths ***************************
+ 
+// console.log(Math);
+// console.log(Math.abs(-6)); // absolut value convert negative to positive integer
+// console.log(Math.round(4.6)); // convert round figure value
+// console.log(Math.ceil(4.2)); // upper value lega
+// console.log(Math.floor(4.2)); // lowest value lega 
+// console.log(Math.min(4,9,2,1,0)); // find minimum value
+// console.log(Math.max(54,32,78,92)); // find maximum value
+
+console.log(Math.random());
+console.log((Math.random()*10)+1); 
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+ 
+ 
+
+
