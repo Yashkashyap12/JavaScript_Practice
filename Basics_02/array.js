434509dbec1fa691776b1eebaa1d15058c6feb0d
Yash Kashyap <gyashkashyap123456@gmail.com>
2024-11-07 19:32:14 +0530

    reupdate

--- Basics_02/array.js
+++ Basics_02/array.js
@@ -1,40 +1,40 @@
 // *************** Array ********************
 // types of declaring array 
 
-// const myArr = [0,1,2,3,4,5]
-// const myHero = ["Radha","Rani"]
+const myArr = [0,1,2,3,4,5]
+const myHero = ["Radha","Rani"]
 
-// const myArr1 = new Array(1,5,6,3)
-// console.log(myArr[3]);
+const myArr1 = new Array(1,5,6,3)
+console.log(myArr[3]);
 
 // ************ Array Methods ****************
 
-// myArr.push(9)
-// myArr.pop()
+myArr.push(9)
+myArr.pop()
 
-// myArr.unshift(9)
-// myArr.shift()
+myArr.unshift(9)
+myArr.shift()
 
-// console.log(myArr.includes(6));
-// console.log(myArr.indexOf(6));
+console.log(myArr.includes(6));
+console.log(myArr.indexOf(6));
 
-// const newArr = myArr.join()
-// console.log(typeof newArr);
-// console.log(myArr);
+const newArr = myArr.join()
+console.log(typeof newArr);
+console.log(myArr);
 
 // *********** Slice ***************
-        // console.log('A', myArr);
+        console.log('A', myArr);
         
-        // const myn1 = myArr.slice(1,3)
+        const myn1 = myArr.slice(1,3)
 
-        // console.log(myn1);
-        // console.log('B', myArr);
+        console.log(myn1);
+        console.log('B', myArr);
 
         // *********** Splice ***************
 
-        // const myn2 = myArr.splice(1,3)
-        // console.log('C', myArr);
-        // console.log(myn1);
+        const myn2 = myArr.splice(1,3)
+        console.log('C', myArr);
+        console.log(myn1);
         
         // *************************************
 
