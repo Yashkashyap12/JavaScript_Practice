a3b9fd564affdd79ccf378f26125ad06be5918db
Yash Kashyap <gyashkashyap123456@gmail.com>
2024-11-07 18:34:41 +0530

    modify

---
+++ Basics_02/array.js
@@ -0,0 +1,41 @@
+// *************** Array ********************
+// types of declaring array 
+
+const myArr = [0,1,2,3,4,5]
+const myHero = ["Radha","Rani"]
+
+const myArr1 = new Array(1,5,6,3)
+console.log(myArr[3]);
+
+// ************ Array Methods ****************
+
+// myArr.push(9)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(6));
+// console.log(myArr.indexOf(6));
+
+// const newArr = myArr.join()
+// console.log(typeof newArr);
+// console.log(myArr);
+
+// *********** Slice ***************
+        console.log('A', myArr);
+        
+        const myn1 = myArr.slice(1,3)
+
+        console.log(myn1);
+        console.log('B', myArr);
+
+        const myn2 = myArr.splice(1,3)
+        console.log('C', myArr);
+        console.log(myn1);
+        
+        
+        
+
+// *********** Splice ***************
+
