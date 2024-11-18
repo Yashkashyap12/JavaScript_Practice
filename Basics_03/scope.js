b4a4e39dc7942fa7a199a85e8dbad14727bf5acd
Yash Kashyap <gyashkashyap123456@gmail.com>
2024-11-18 19:14:40 +0530

    modify changes

--- Basics_03/scope.js
+++ Basics_03/scope.js
@@ -3,9 +3,46 @@ let a = 300
 if(true){
     let a = 10
     const b = 20 // Block Scope 
-    console.log("Inner : ",a);
+    // console.log("Inner : ",a);
     
 } // it is called scope 
 
- console.log(a);
+//  console.log(a);
  
+function one () {
+    const username = "Yash"
+
+    function two (){
+        const website = "yash.com"
+        console.log(username);
+        
+    }
+    // console.log(website);
+
+    two()   
+}
+// one()
+
+if (true) {
+    const username = "Yash Kashyap"
+    if (username === "Yash Kashyap") {
+        const website = " yash.com"
+        // console.log(username + website);   
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+// ******************** Interesting *************************
+
+console.log(addOne(5))
+function addOne (num) {
+    return num + 1   
+}
+
+
+console.log(addTwo(5))  // is called hoisting 
+const addTwo = function (num){ // sometime we will say expression
+    return num + 2
+}
