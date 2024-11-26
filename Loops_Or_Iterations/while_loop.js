c079a9474872f945666bbe59a2da716a2ce0af55
Yash Kashyap <gyashkashyap123456@gmail.com>
2024-11-26 19:13:18 +0530

    add whileloop file

---
+++ Loops_Or_Iterations/while_loop.js
@@ -0,0 +1,22 @@
+// ************ While Loop ************
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArr = ['Radha','Kanha','Shyam']
+// let arr = 0
+// while (arr < myArr.length) {
+//     console.log(`Value is ${myArr[arr]}`);
+//     arr = arr + 1
+// }
+
+// ********* Do While ************
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
